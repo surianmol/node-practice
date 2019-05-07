18779bb2b575740d0cf6576b1342a99c8dcf672e
Anmol Suri <anmol.s@corp.cumulus>
2019-05-07 15:17:29 +0530

    Adding webpack config for nodemon and babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,40 @@
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const nodemonPlugin = require('nodemon-webpack-plugin');
 
-module.exports = {
+module.exports = (env = {}) => {
+  const config = {
     target: 'node',
+    mode: env.development ? 'development' : 'production',
     entry: './src/index.js',
     output: {
       filename: 'bundle.js'
     },
-    devtool: 'inline-source-map',
-    plugins: [
-        new CleanWebpackPlugin()
-    ]
+    devtool: env.development ? 'cheap-eval-source-map' : false,
+    resolve: {
+      // tells Webpack what files to watch.
+      modules: ['node_modules', 'src']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    },
+    plugins: [new CleanWebpackPlugin()]
+  };
+
+  if (env.nodemon) {
+    config.watch = true;
+    config.plugins.push(new nodemonPlugin());
+  }
+
+  return config;
 };
